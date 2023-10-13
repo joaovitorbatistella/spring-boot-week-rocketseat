dfa93f42422c4e9a4f64f3a3dd2ce1f105a4e9aa
Joao Vitor batistella <joaovitor.batistella.ifrs@gmail.com>
2023-10-13 00:12:24 -0300

    Aula 3

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-week-rocketseat
---
+++ src/test/java/br/com/joaovitorbatistella/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaovitorbatistella.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
