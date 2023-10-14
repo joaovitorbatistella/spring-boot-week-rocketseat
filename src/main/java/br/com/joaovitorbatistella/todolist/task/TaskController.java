b225c03a33c43032b38597c066434b439ad77744
Joao Vitor batistella <joaovitor.batistella.ifrs@gmail.com>
2023-10-13 22:47:30 -0300

    Aula 5 pt1

--- src/main/java/br/com/joaovitorbatistella/todolist/task/TaskController.java
+++ src/main/java/br/com/joaovitorbatistella/todolist/task/TaskController.java
@@ -1,10 +1,22 @@
 package br.com.joaovitorbatistella.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.joaovitorbatistella.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,10 +26,51 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
-        System.out.println("Arrive on controller");
-        var task = this.taskRepository.save(taskModel);
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
 
-        return task;        
+        var currentDate = LocalDateTime.now();
+
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("The start and end date should be bigger than current date");
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("The start date should be less than current date");
+        }
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);        
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if(task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Task not found.");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("User has no permission to update this task.");
+        }
+
+        Utils.copyNonNullPropeties(taskModel, task);
+
+        return ResponseEntity.status(HttpStatus.OK).body(this.taskRepository.save(task));
     }
 }
--- src/main/java/br/com/joaovitorbatistella/todolist/task/TaskModel.java
+++ src/main/java/br/com/joaovitorbatistella/todolist/task/TaskModel.java
@@ -30,4 +30,11 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;    
+
+    public void setTitle(String title) throws Exception {
+        if(title.length() > 50) {
+            throw new Exception("The 'title' field length should be less then 50 characters");
+        }
+        this.title = title;
+    }
 }
